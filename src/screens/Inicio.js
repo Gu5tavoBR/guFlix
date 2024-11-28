03cc94593d416b1bc7e8afbc8172c43292228609
Gu5tavoBR <gbranco982@gmail.com>
2024-11-28 18:39:50 -0300

    Bug na imagem de cartaz

--- src/screens/Inicio.js
+++ src/screens/Inicio.js
@@ -1,7 +1,8 @@
 import React from "react";
-import { Tela, BuscaContainer, Input, Buscar } from "./StylesInicio";
+import { Tela, BuscaContainer, Input, Buscar, Titulo, Banner, BannerImg } from "./StylesInicio";
 import Header from "../components/Header";
 import { Feather } from "@expo/vector-icons"
+import { ScrollView, Image } from "react-native";
 
 export default function Inicio() {
   return (
@@ -18,6 +19,16 @@ placeholderTextColor="#ddd"
 <Feather name="search" size={30} color="white" />
 </Buscar>
 </BuscaContainer>
+
+<ScrollView>
+<Titulo>Em cartaz</Titulo>
+
+<Banner>
+<Image
+source={{url: "https://images.app.goo.gl/GmZmG4e3xNFvCz7w5"}}
+/>
+</Banner>
+</ScrollView>
     </Tela>
   );
 }
--- src/screens/StylesInicio.js
+++ src/screens/StylesInicio.js
@@ -1,33 +1,52 @@
 import styled from "styled-components/native";
 
 export const Tela = styled.View`
-background-color: #141a29;
+  background-color: #141a29;
   flex: 1;
   padding: 4px 0;
 `;
 
 export const BuscaContainer = styled.View`
-flex-direction: row;
-width: 100%;
-height: 50px;
+  flex-direction: row;
+  width: 100%;
+  height: 50px;
   align-items: center;
   padding: 0 14px;
-margin-bottom: 8px;
+  margin-bottom: 8px;
 `;
 
 export const Input = styled.TextInput`
-background-color: rgba(255, 255, 255, 0.5);
-width: 85%;
-border-radius: 50px;
+  background-color: rgba(255, 255, 255, 0.5);
+  width: 85%;
+  border-radius: 50px;
   padding: 8px 15px;
-font-size: 18;
+  font-size: 18;
   color: #fff;
   height: 50px;
 `;
 
 export const Buscar = styled.TouchableOpacity`
-width: 15%;
+  width: 15%;
   height: 50px;
-align-items: center;
+  align-items: center;
   justify-content: center;
+`;
+
+export const Titulo = styled.Text`
+  padding-top: 20px;
+  padding-bottom: 8px;
+  font-size: 24px;
+  font-weight: bold;
+  color: #fff;
+padding-left: 14px;
+padding-right: 14px;
+`;
+
+export const Banner = styled.TouchableOpacity`
+height: 1000px;
+`;
+
+export const BannerImg = styled.Image`
+height: 150px;
+border-radius: 6px;
 `;
